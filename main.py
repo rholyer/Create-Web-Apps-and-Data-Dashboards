4786c083d4c77022befd6b0875eb22a9333fcfef
rholyer <rholyer@gmail.com>
2025-02-23 22:11:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''Look at you go! \n
+Coding like a pro!
+''')

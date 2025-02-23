615e6311a1807474a4376b4bc21f9b2950bca94b
rholyer <rholyer@gmail.com>
2025-02-23 22:21:05 +0000

    Add table

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
+data = {
+  'Series_1':[1,3,4,5,7],
+  'Series_2':[10,30,40,100,250],
+}
+
+df = pandas.DataFrame(data)
+
 st.title('Streamlit App')
 st.subheader('Introducing Streamlit in Automate Everything with Python')
 st.write('''Look at you go! \n
 Coding like a pro!
+{{{df}}}
 ''')
+st.write(df)
